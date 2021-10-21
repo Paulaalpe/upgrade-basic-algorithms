aaedfb9dfb93bf4fa4f39fb54b6c43acfdc767ad
Paula Alvarez <paulaalpe@gmail.com>
2021-10-21 23:08:53 +0200

    Iteration#4

--- index.js
+++ index.js
@@ -48,3 +48,34 @@ x = y + z;
 
 //1.5
 x = y * z;
+
+// // Iteración #4: Arrays
+
+//1.1 Consigue el valor "HULK" del array y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+
+//1.2 Cambia el primer elemento de avengers a "IRONMAN"
+//Nota: aquí se podría usar tb: //avengers.shift(); avengers.unshift("IRONMAN");
+avengers.splice(0,1,"IRONMAN");
+
+//1.3 Alert numero de elementos en el array usando la propiedad correcta de Array.
+alert(avengers.length);
+
+//1.4 Añade 2 elementos al array: "Morty" y "Summer". Muestra en consola el último personaje del array 
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters[4]);
+
+//1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+//Nota: renombro array porque sino me sale error Uncaught SyntaxError: Identifier 'rickAndMortyCharacters' has already been declared
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters2.pop();
+console.log(rickAndMortyCharacters2[0]);
+console.log(rickAndMortyCharacters2[4]);
+
+//1.6 Elimina el segundo elemento del array y muestra el array por consola.
+rickAndMortyCharacters2.splice(1,1);
+console.log(rickAndMortyCharacters2);
+
+
